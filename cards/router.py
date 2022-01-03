98fdc8aed9d32ffea9e3fdaab0cd98f6e802ba44
Ömer Atalay <omer@atalay.dev>
2022-01-03 23:35:42 +0300

    use ModeViewSet to avoid code-duplication

--- cards/router.py
+++ cards/router.py
@@ -2,4 +2,4 @@ from cards.viewsets.card import CardViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'cards', CardViewSet)
+router.register(r'cards', CardViewSet, basename='cards')
--- cards/serializers/card.py
+++ cards/serializers/card.py
@@ -6,6 +6,3 @@ class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = ['id', 'front', 'back', 'streak', 'created_at']
-
-    def create(self, validated_data):
-        return self.Meta.model.objects.create(**validated_data)
