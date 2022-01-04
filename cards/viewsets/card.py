19afd4a3f7b7a294b29031bc735f903c0bfa1061
Ömer Atalay <omer@atalay.dev>
2022-01-04 22:55:38 +0300

    override base class queryset instead of calling from a function

--- cards/viewsets/card.py
+++ cards/viewsets/card.py
@@ -4,7 +4,6 @@ from cards.models import Card
 from cards.serializers.card import CardSerializer
 
 class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.order_by('streak', 'created_at')
     serializer_class = CardSerializer
 
-    def get_queryset(self):
-        return Card.objects.order_by('streak', 'created_at')
