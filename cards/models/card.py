f6475b88439777ff3fb4b31d6a318da91798a9ea
Ömer Atalay <omer@atalay.dev>
2022-01-11 22:25:03 +0300

    add confidance level and update status values to logical ones

--- cards/models/card.py
+++ cards/models/card.py
@@ -6,15 +6,23 @@ from django.contrib.auth.models import User
 class Card(models.Model):
     
     class CardStatus(models.IntegerChoices):
-        NEW = 0
-        ON_GOING = 1
-        DONE = 2
+        ARCHIVED = 0
+        ACTIVE = 1
+        COMPLETED = 2
+
+    class ConfidenceLevel(models.IntegerChoices):
+        EXCELLENT = 5
+        GOOD = 4
+        AVERAGE = 3
+        FAIR = 2
+        POOR = 1
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     front = models.TextField(max_length=256, blank=False, verbose_name='Front-face')
     back = models.TextField(max_length=256, blank=False, verbose_name='Back-face')
     streak = models.IntegerField(default=0, blank=False)
-    status = models.IntegerField(choices=CardStatus.choices, default=CardStatus.NEW)
+    status = models.IntegerField(choices=CardStatus.choices, blank=False, default=CardStatus.ACTIVE)
+    confidence = models.IntegerField(choices=ConfidenceLevel.choices, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, blank=False, on_delete=models.DO_NOTHING)
 
