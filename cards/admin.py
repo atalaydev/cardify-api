ab3dc509403ff9a7fc993fc9c2d3be2f73ea7b40
Ömer Atalay <omer@atalay.dev>
2022-01-15 13:56:40 +0300

    delete admin implementations

--- cards/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from cards.models import Card
-
-
-class CardAdmin(admin.ModelAdmin):
-    list_display = ['id', 'front', 'streak', 'created_at']
-
-admin.site.register(Card, CardAdmin)
